5cfd5d6c22e36cd2d9eb15fe5087b4d490a53f21
aaldnq <pirat.abdic1@gmail.com>
2026-05-02 23:26:37 +0200

    Initial commit: first steps done

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 #define MAX_ITEMS 16
 
 typedef struct {
-    char name[MAX_NAME]:
+    char name[MAX_NAME];
     float weight;
     int value;
 } Item;
 
 void print_item(Item item) {
     printf("%-20s | weight: %.1f | value: %d gold\n", 
-        item.name, item.wieght, item.value);
+        item.name, item.weight, item.value);
 }
 
 
@@ -21,8 +21,16 @@ void print_item(Item item) {
 
 int main(){
     Item sword;
+    Item potion;
     strcpy(sword.name, "Iron Sword");
-    
+    sword.weight = 3.5f;
+    sword.value = 40;
 
+    strcpy(potion.name, "Health Potion");
+    potion.weight = 0.5f;
+    potion.value = 15;
+
+    print_item(sword);
+    print_item(potion);
     return 0;
 }
